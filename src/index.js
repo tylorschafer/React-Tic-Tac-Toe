f4126ad49dbd292ed7018d9442ff772eb38ac9c2
Tylor Schafer <tylorschafer@gmail.com>
2019-11-26 18:13:10 -0700

    Add xIsNext state to change between player markings

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    square[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
